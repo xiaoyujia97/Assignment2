- Fixed precision calculations (indent error) - Refactoring

Assignment2/models/documents_folder.py
@@ -1,4 +1,5 @@
-import glob, os
+import glob
+import os
 import numpy as np
 from models.document import Document
 from collections import defaultdict
@@ -24,15 +25,12 @@ class DocumentsFolder:
 
         self.corresponding_query = queries_dictionary[self.folder_number]
 
-
         # ranking results
         self.bm25_ranking_result = {}
 
         self.jm_ranking_result = {}
 
         self.prm_ranking_result = {}
-
-
 
     def load_documents_in_location(self):
         documents_dictionary = {}
@@ -51,7 +49,7 @@ class DocumentsFolder:
 
         # get the length of all documents
         sum_document_lengths = sum(doc.get_document_length()
-                                    for doc in self.documents.values())
+                                   for doc in self.documents.values())
 
         # calculate the average
         avg_doc_len = sum_document_lengths / len(self.documents)
@@ -75,14 +73,9 @@ class DocumentsFolder:
 
         return document_frequencies, relevant_document_frequencies
 
-    def bm25_ranking(self, using_relevance : bool = True,
+    def bm25_ranking(self, using_relevance: bool = True,
                      K1: float = 1.2, K2: float = 500.0,
                      B: float = 0.75):
-        # declare the variables
-        # K1 = 1.2
-        # K2 = 500
-        # B = 0.75
-
 
         # initialise the dictionary
         document_weighting = {}
@@ -97,16 +90,23 @@ class DocumentsFolder:
             # N is the number of documents in the folder
             N = len(self.documents)
 
+            """
+            The BM25 function is comprised of several key components.
+            
+            The first (inverse document frequency) comprises term 1.  It is designed to measure the amount
+            of useful information that each term provides.
+            
+            There is also term frequency components measuring the frequency of terms in both the query &
+            document.  The document frequency has length normalisation K that ensures longer documents do not
+            benefit from a larger score due to having more words.
+            """
+
             # for query_term, query_frequency in self.corresponding_query.parsed_query_text.items():
             for query_term, query_frequency in self.corresponding_query.parsed_long_query.items():
                 if query_term in document.terms:
                     fi = document.terms[query_term]
                 else:
                     fi = 0
-                # TODO:
-                """
-                Description of the BM25 ranking function
-                """
 
                 # ri is the appearance of the term in relevant documents
                 if query_term in self.relevant_document_frequencies:
@@ -115,22 +115,17 @@ class DocumentsFolder:
                     ri = 0
 
                 # ni is document frequency of the term
-                if query_term  in self.document_frequencies:
+                if query_term in self.document_frequencies:
                     ni = self.document_frequencies[query_term]
                 else:
                     ni = 0
 
-
-                # calculate each term individually
-                term_1_num = (ri + 0.5)/(R - ri + 0.5)
-                term_1_denom = (ni - ri + 0.5)/(N - ni - R + ri + 0.5)
-
                 if using_relevance:
-                    term_1_num = (ri + 0.5) / (R - ri + 0.5)
-                    term_1_denom = (ni - ri + 0.5) / (N - ni - R + ri + 0.5)
+                    term_1_numerator = (ri + 0.5) / (R - ri + 0.5)
+                    term_1_denominator = (ni - ri + 0.5) / (N - ni - R + ri + 0.5)
                 else:
-                    term_1_num = 1
-                    term_1_denom = (ni - 0 + 0.5) / (N - ni - 0 + 0.5)
+                    term_1_numerator = 1
+                    term_1_denominator = (ni - 0 + 0.5) / (N - ni - 0 + 0.5)
 
                 '''
                 We are seeking to make sure the fraction is greater than 1 in order to guarantee
@@ -164,75 +159,41 @@ class DocumentsFolder:
                 '''
 
                 if using_relevance:
-                    term_1 = term_1_num/term_1_denom * 2*(N-R+1)/(0.5/(R+0.5))
+                    term_1 = term_1_numerator / term_1_denominator * 2 * (N - R + 1) / (0.5 / (R + 0.5))
                 else:
-                    term_1 = term_1_num/term_1_denom * 2*(N+1)
-
-
+                    term_1 = term_1_numerator / term_1_denominator * 2 * (N + 1)
 
                 term_2 = (K1 + 1) * fi / (K + fi)
 
                 term_3 = (K2 + 1) * query_frequency / (K2 + query_frequency)
 
+                # put it all together and add to the current weighting
                 weighting += np.log10(term_1) * term_2 * term_3
 
             document_weighting[document_id] = weighting
 
-        ranked_results = {k: v for k, v
-                                        in sorted(document_weighting.items(),
-                                              key= lambda item: item[1], reverse = True)}
+        ranked_results = {k: v for k, v in sorted(document_weighting.items(),
+                                                  key=lambda item: item[1],
+                                                  reverse=True)}
 
         return ranked_results
-
-
 
     def jm_lm_ranking(self):
         # TODO: complete the Jelinek-Mercer based Language Model
         print()
 
-    def prm_ranking(self, K1: float = 1.2, K2: float = 500, B: float = 0.75, n_top_docs : int = 3):
+    def prm_ranking(self, K1: float = 1.2, K2: float = 500, B: float = 0.75, n_top_docs: int = 3):
 
         # Pseudo-Feedback Algorithm
 
         # should this be used or should the long query be used??????
-        original_query = self.corresponding_query.parsed_query_text
-
+        # original_query = self.corresponding_query.parsed_query_text
 
         # Initialize the dictionary for document weights
-        document_weighting = self.bm25_ranking(using_relevance=False, K1 = K1,
-                                               K2 = K2, B = B)
-
-
-
-        # # 1. Rank documents using the query likelihood score for query Q (BM25)
-        # for document_id, document in self.documents.items():
-        #     # Initialize the weighting and the value of K
-        #     weighting = 0
-        #     K = K1 * ((1 - B) + B * document.get_document_length() / self.avg_document_length)
-        #     # R: number of relevant documents
-        #     R = sum(self.relevance_for_folder.values())
-        #     # N: number of documents in the folder
-        #     N = len(self.documents)
-        #
-        #     for query_term, query_frequency in original_query.items():
-        #         fi = document.terms.get(query_term, 0)
-        #         ri = self.relevant_document_frequencies.get(query_term, 0)
-        #         ni = self.document_frequencies.get(query_term, 0)
-        #
-        #         term_1_num = 1
-        #         term_1_denom = (N - ni + 0.5) / (N - ni - R + ri + 0.5)
-        #         term_1 = term_1_num / term_1_denom if term_1_denom != 0 else 0
-        #
-        #         term_2 = (K1 + 1) * fi / (K + fi)
-        #         term_3 = (K2 + 1) * query_frequency / (K2 + query_frequency)
-        #
-        #         if term_1 * term_2 * term_3 > 0:
-        #             weighting += np.log(term_1 * term_2 * term_3)
-        #
-        #     document_weighting[document_id] = weighting
+        document_weighting = self.bm25_ranking(using_relevance=False, K1=K1,
+                                               K2=K2, B=B)
 
         # 2. Select some number of the top-ranked documents to be the set C (top 3 documents)
-
 
         sorted_document_weighting = sorted(document_weighting.items(), key=lambda x: x[1], reverse=True)
         top_k_documents = [doc_id for doc_id, _ in sorted_document_weighting[:n_top_docs]]
@@ -258,18 +219,16 @@ class DocumentsFolder:
                 if p_w_D > 0:
                     kl_divergence += p_w_R * np.log(p_w_R / p_w_D)
                 else:
-                    #divide by 0.01 to avoid case of divide by 0 or less
+                    # divide by 0.01 to avoid case of divide by 0 or less
                     kl_divergence += p_w_R * np.log(p_w_R / 0.0001)
 
             document_weighting[document_id] = 0 - kl_divergence
 
-        self.prm_ranking_result = {k: v for k, v 
-                                    in sorted(document_weighting.items(),
-                                            key=lambda item: item[1], reverse=True)}
-        
+        self.prm_ranking_result = {k: v for k, v
+                                   in sorted(document_weighting.items(),
+                                             key=lambda item: item[1], reverse=True)}
 
     def calculate_average_precision(self):
-
 
         num_relevant_documents = sum(self.relevance_for_folder.values())
 
@@ -281,12 +240,10 @@ class DocumentsFolder:
             # check if above threshold for bm25
             if self.relevance_for_folder[document_id]:
                 bm25_running_relevant_docs += 1
-                bm25_current_precision = bm25_running_relevant_docs/current_iteration
+                bm25_current_precision = bm25_running_relevant_docs / current_iteration
                 bm25_rolling_average_precision += bm25_current_precision
 
             current_iteration += 1
-
-
 
         bm25_average_precision = bm25_rolling_average_precision / num_relevant_documents
 
@@ -298,11 +255,10 @@ class DocumentsFolder:
             # check if above threshold for bm25
             if self.relevance_for_folder[document_id]:
                 jm_running_relevant_docs += 1
+                jm_current_precision = jm_running_relevant_docs / current_iteration
+                jm_rolling_average_precision += jm_current_precision
 
-            jm_current_precision = jm_running_relevant_docs / current_iteration
             current_iteration += 1
-
-            jm_rolling_average_precision += jm_current_precision
 
         jm_average_precision = jm_rolling_average_precision / num_relevant_documents
 
@@ -314,11 +270,10 @@ class DocumentsFolder:
             # check if above threshold for bm25
             if self.relevance_for_folder[document_id]:
                 prm_running_relevant_docs += 1
+                prm_current_precision = prm_running_relevant_docs / current_iteration
+                prm_rolling_average_precision += prm_current_precision
 
-            prm_current_precision = prm_running_relevant_docs / current_iteration
             current_iteration += 1
-
-            prm_rolling_average_precision += prm_current_precision
 
         prm_average_precision = prm_rolling_average_precision / num_relevant_documents
 
@@ -328,8 +283,7 @@ class DocumentsFolder:
                                          jm_average_precision,
                                          prm_average_precision)
 
-
-    def calculate_precision_at_k(self, k = 10):
+    def calculate_precision_at_k(self, k=10):
 
         bm25_predicted_relevance = 0
         jm_predicted_relevance = 0
@@ -362,7 +316,6 @@ class DocumentsFolder:
                                       jlm_precision_at_k,
                                       prm_precision_at_k)
 
-
     def calculate_discounted_cumulative_gain(self):
         bm25_discounted_cumulative_gain = 0
         jm_discounted_cumulative_gain = 0
@@ -374,7 +327,7 @@ class DocumentsFolder:
                 if i == 1:
                     bm25_discounted_cumulative_gain += 1
                 else:
-                    bm25_discounted_cumulative_gain += 1/np.log(i)
+                    bm25_discounted_cumulative_gain += 1 / np.log(i)
 
             # increment
             i += 1